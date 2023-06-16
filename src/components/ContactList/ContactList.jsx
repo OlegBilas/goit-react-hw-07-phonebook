dc9975ea7f033bd7ce4831098403143c00909440
Oleg <olegbilas@gmail.com>
2023-06-16 13:35:07 +0300

    createSelector доданий

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,27 +1,16 @@
 import { List, Item, Name, Number } from './ContactList.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from 'redux/operations';
-import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectFilteredContacts } from 'redux/selectors';
 
 const ContactList = () => {
-  const filter = useSelector(selectFilter);
-  const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
 
-  const filteredContacts = () => {
-    if (filter) {
-      const normalizedFilter = filter.toLowerCase();
-      return contacts.filter(contact =>
-        contact.name.toLowerCase().includes(normalizedFilter)
-      );
-    } else {
-      return contacts;
-    }
-  };
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <List>
-      {filteredContacts().map(({ id, name, phone }) => (
+      {filteredContacts.map(({ id, name, phone }) => (
         <Item key={id}>
           <Name>{name}:</Name>
           <Number>{phone}</Number>
