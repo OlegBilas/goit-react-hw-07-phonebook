e7f633d5f6a69e717d67a48a045b7c375b715777
Oleg <olegbilas@gmail.com>
2023-06-16 13:49:52 +0300

    Update App.styled.jsx

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   max-width: calc(100% - 40px);
+  height: 100vh;
   padding: 20px;
-  margin: 20px auto;
   background-color: #a9c2f5;
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
-  background-image: url('../images/phone_1920.jpg');
+  background-image: url('images/phone_1920.jpg');
 `;
 
 export const TitlePhonebook = styled.h1`
